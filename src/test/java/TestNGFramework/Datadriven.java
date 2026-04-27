19fb3c9cfa1998f8814f150f621ccaf35f6f9d5c
suresh <rudrasuresh98@gmail.com>
2026-04-27 13:57:40 +0530

    tuesday push

--- src/test/java/TestNGFramework/Datadriven.java
+++ src/test/java/TestNGFramework/Datadriven.java
@@ -1,19 +1,41 @@
 package TestNGFramework;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class Datadriven {
-	@DataProvider(name="sending")
-	public static Object prodider() {
-		return new Object[][] {
-	        { "user1", "pass1" },
-	        { "user2", "pass2" }
-	    };
-	}
-	@Test(dataProvider="sending")
-	public void Taking(String a,String b) {
-		System.out.println(a+"   "+b);
+//	@DataProvider(name="sending")
+//	public static Object[][] prodider() throws Throwable {
+//		FileInputStream fis =new FileInputStream("C:\\Users\\Suresh Bathula\\Documents\\DataDriven");
+//		XSSFWorkbook wb=new XSSFWorkbook(fis);
+//		XSSFSheet sheet=wb.getSheet("Surya");
+//		int row=sheet.getPhysicalNumberOfRows();
+//		int colomn=sheet.getRow(0).getPhysicalNumberOfCells();
+//		
+//		
+//		return ;
+//		
+//	}
+	@Test
+	public void Taking() throws IOException {
+		FileInputStream fis =new FileInputStream("C:\\Users\\Suresh Bathula\\Desktop\\productiondata.xlsx");
+		XSSFWorkbook wb=new XSSFWorkbook(fis);
+		XSSFSheet sheet=wb.getSheet("ProdData");
+		int row=sheet.getPhysicalNumberOfRows();
+		int colomn=sheet.getRow(0).getPhysicalNumberOfCells();
+		for(int i=1;i<row;i++) {
+			for(int j=0;j<colomn;j++) {
+				System.out.print(sheet.getRow(i).getCell(j).toString()+"  ");
+			}
+			System.out.println();
+		}
+		
 	}
 
 }
